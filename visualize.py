daf95ca2000584cdec8ca5d133170d87412f251b
Lukas Pelz <61017369+lukpausr@users.noreply.github.com>
2021-01-06 18:07:41 +0100

    Added Methods for reusability

--- visualize.py
+++ visualize.py
@@ -5,73 +5,167 @@ import os
 import gpxpy
 import numpy as np
 
-path = "Z:/2020-JG18-T31Bewegungsanalyse-Pelz-Kroener/05-Messfahrten_Daten/Messfahrt2.gpx"
+def readPoints(path, jumpSize = 1):
+    gpx_file = open(path, 'r')
+    gpx = gpxpy.parse(gpx_file)
+    i = 1
+    lats = []
+    lons = []
+    times = []
+    elev = []
+    track = gpx.tracks[0]
+    for segment in track.segments:
+        for point in segment.points:
+            if i % jumpSize == 0:
+                lats.append(point.latitude)
+                lons.append(point.longitude)
+                times.append(point.time)
+                elev.append(point.elevation)
+            i = i+1
+    print(len(lats))
+    return lats, lons, times, elev
 
-gpx_file = open(path, 'r')
-gpx = gpxpy.parse(gpx_file)
-i = 1
-lats = []
-lons = []
-times = []
-elev = []
-track = gpx.tracks[0]
-for segment in track.segments:
-    for point in segment.points:
-        if True: #i % 2 == 1:
-            lats.append(point.latitude)
-            lons.append(point.longitude)
-            times.append(point.time)
-            elev.append(point.elevation)
-        i = i+1
+def calculateDistance(lats, lons):
+    #Distanzen in Listen speichern
+    distances_total = [0]
+    distances_diff = [0]
+    
+    total = 0
+    for i in range(1, len(lats)):
+        dist = calculate.calc_distance(lats[i], lons[i], lats[i-1], lons[i-1])
+        total = total + dist
+        distances_total.append(total)
+        distances_diff.append(dist)
+        
+    return distances_total, distances_diff
 
-print(len(lats))
+def calculateTime(times):
+    #Startzeitpunkt ermitteln
+    begin = (times[0].second + times[0].minute * 60 + times[0].hour * 3600)
+    
+    #Zeitpunkte in Listen speichern
+    times_total = [0]
+    times_diff = [0]        
+           
+    for i in range(1,len(times)):
+        times_total.append((times[i].second + times[i].minute * 60 + times[i].hour * 3600) - begin)
+        times_diff.append((times[i].second + times[i].minute * 60 + times[i].hour * 3600)-(times[i-1].second + times[i-1].minute * 60 + times[i-1].hour * 3600))
+    
+    return begin, times_total, times_diff
 
-#Distanzen in Listen speichern
-distances_total = [0]
-distances_diff = [0]
+def calculateElevation(distances_diff, elev):
+    #Steigungen berechnen
+    slopes = [0]
+    for i in range(1, len(distances_diff)-1):
+        m = (elev[i] - elev[i-1]) / (distances_diff[i])
+        slopes.append(np.arctan(m)*100)
+    slopes.append(0)
+    
+    return slopes
+    
+def calculateVelocity(distances_diff, times_diff):
+    #Berechnen der Geschwindigkeiten
+    velocities = [0]
+    #in km/h
+    for i in range(1, len(distances_diff)):
+        velocities.append(distances_diff[i]*3.6/(times_diff[i]))
+        
+    return velocities
 
-total = 0
-for i in range(1, len(lats)):
-    dist = calculate.calc_distance(lats[i], lons[i], lats[i-1], lons[i-1])
-    total = total + dist
-    distances_total.append(total)
-    distances_diff.append(dist)
+def calculateAcceleration(velocities, times_diff):
+    #Berechnen der Beschleunigungen
+    accelerations = [0]
+    #in m/s^2
+    for i in range(1, len(velocities)):
+        accelerations.append((velocities[i] - velocities[i-1]) / (3.6*(times_diff[i])))
+        
+    return accelerations
 
+def calculateValues(path, jumpSize = 1):
+    
+    lats, lons, times, elev = readPoints(path, jumpSize)    
+    distances_total, distances_diff = calculateDistance(lats, lons)   
+    begin, times_total, times_diff = calculateTime(times)  
+    slopes = calculateElevation(distances_diff, elev)  
+    velocities = calculateVelocity(distances_diff, times_diff)
+    accelerations = calculateAcceleration(velocities, times_diff)
+    
+    return Result(lats, lons, times, elev, distances_total, distances_diff, begin, times_total, times_diff, slopes, velocities, accelerations)
 
-#Startzeitpunkt ermitteln
-begin = (times[0].second + times[0].minute * 60 + times[0].hour * 3600)
+def printMultiplot(x_axis, y_axis, x_label, y_label, label, save_path=None):
+    
+    plt.rcParams["figure.figsize"] = (20,8)
+    
+    for i in range(len(x_axis)):
+        plt.plot(x_axis[i], y_axis[i], label=label[i])
+        
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
+    plt.legend(loc='best')
 
-#Zeitpunkte in Listen speichern
-times_total = [0]
-times_diff = [0]        
-       
-for i in range(1,len(times)):
-    times_total.append((times[i].second + times[i].minute * 60 + times[i].hour * 3600) - begin)
-    times_diff.append((times[i].second + times[i].minute * 60 + times[i].hour * 3600)-(times[i-1].second + times[i-1].minute * 60 + times[i-1].hour * 3600))
+    if save_path is None:
+        plt.show()
+    else:
+        destination = save_path + "/" + x_label + y_label + ".pdf"
+        print(destination)
+        plt.savefig(destination, dpi=1200, bbox_inches='tight')
+    
+    plt.show()
+        
 
-
-#Steigungen berechnen
-slopes = [0]
-for i in range(1, len(distances_diff)-1):
-    m = (elev[i] - elev[i-1]) / (distances_diff[i])
-    slopes.append(np.arctan(m)*100)
-slopes.append(0)
-
-
-#Berechnen der Geschwindigkeiten
-velocities = [0]
-#in km/h
-for i in range(1, len(distances_diff)):
-    velocities.append(distances_diff[i]*3.6/(times_diff[i]))
-
-#Berechnen der Beschleunigungen
-accelerations = [0]
-#in m/s^2
-for i in range(1, len(velocities)):
-    accelerations.append((velocities[i] - velocities[i-1]) / (3.6*(times_diff[i])))
-
+class Result:    
+    def __init__(self, lats, lons, times, elev, distances_total, distances_diff, begin, times_total, times_diff, slopes, velocities, accelerations):
+        self.lats = lats
+        self.lons = lons
+        self.times = times
+        self.elev = elev
+        self.distances_total = distances_total
+        self.distances_diff = distances_diff
+        self.begin = begin
+        self.times_total = times_total
+        self.times_diff = times_diff
+        self.slopes = slopes
+        self.velocities = velocities
+        self.accelerations = accelerations
+    
 
 import matplotlib.pyplot as plt # v 3.0.3
+
+path = "Z:/2020-JG18-T31Bewegungsanalyse-Pelz-Kroener/05-Messfahrten_Daten/Messfahrt2.gpx"
+save_path = "B:\Privat\Studium\Studienarbeit\dataprep_TripLog-main" 
+
+values = []
+
+values.append(calculateValues(path, 1))
+values.append(calculateValues(path, 2))
+values.append(calculateValues(path, 5))
+
+jump_sizes = ["Alle 3s", "Alle 6s", "Alle 15s"]
+
+
+speed = []
+distance = []
+times = []
+acceleration = []
+for i in range(len(values)):
+    speed.append(values[i].velocities)
+    times.append(values[i].times_total)
+    distance.append(values[i].distances_total)
+    acceleration.append(values[i].accelerations)
+    
+printMultiplot(times, speed, "Zeit", "Geschwindigkeit", jump_sizes)
+
+printMultiplot(times, distance, "Zeit", "Strecke", jump_sizes)
+
+printMultiplot(distance, speed, "Strecke", "Geschwindigkeit", jump_sizes)
+
+printMultiplot(times, acceleration, "Zeit", "Beschleunigung", jump_sizes, save_path)
+
+
+
+
+
+
 # #v_s
 # plt.figure(figsize=(15, 6), dpi=80)
 # plt.subplot(111)
