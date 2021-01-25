a270ac6b292fdde7997e148e21f67d027fc7af43
Lukas Pelz <61017369+lukpausr@users.noreply.github.com>
2021-01-25 20:16:49 +0100

    Add files via upload

--- data_import.py
+++ data_import.py
@@ -3,6 +3,25 @@ import pandas as pd
 import calculate
 import numpy as np 
 
+class Record:
+    def __init__(self, path_gps, path_sensor, label = "", sublabel = "", subsublabel = ""):
+        self.path_gps = path_gps
+        self.path_sensor = path_sensor
+        self.label = label
+        self.sublabel = sublabel
+        self.subsublabel = subsublabel
+        self.splitted_gps = []
+        self.splitted_sensor = []
+        
+    def __str__(self):
+        print("### Record Object ###")
+        print("GPS-Path: ", self.path_gps)
+        print("Sensor-Path: ", self.sensor_gps)
+        print("Label: ", self.label)
+        print("Sublabel: ", self.sublabel)
+        print("SubSubLabel: ", self.subsublabel)
+        print("#####################")
+
 def csv_import(path):
     """
     Task: Importieren und Verarbeiten einer CSV Datei
@@ -30,6 +49,7 @@ def data_import(path):
     None.
 
     """
+    records = []
     # Durchläuft alle User-Ordner
     for files in os.listdir(path):
         # Durchläuft alle Dateien in den User-Ordnern
@@ -74,9 +94,91 @@ def data_import(path):
                 
 
     return(ml_csv)
+    # return(records)
     # print(ml_csv) 
 
+def preperate_gps(record):
+# =============================================================================
+# Alles was mit GPS Dateien zu tun hat - schneiden der Dateien, Berechnen der
+# Features etc
+# =============================================================================
+    
+    # DataFrame erstellen
+    try:
+        # print(path+files+"/"+data)
+        dataFrame = csv_import(record.path_gps)
+    except: 
+        print("INVALID DATA: ", record.path_gps)
+        dataFrame = pd.DataFrame(columns=["Average speed", "Maximum speed", "Average speed without waiting","Minimum acceleration", "Maximum acceleration", "Duration", "Distance"])
 
+
+def preperate_sensor(record):
+# =============================================================================
+# Alles was mit Sensor Dateien zu tun hat - Resampling, Berechnen von
+# Features etc
+# =============================================================================
+
+    if os.path.isfile(record.path_sensor):
+        df = pd.read_csv(record.path_sensor, sep = ",")
+        # print(df.head(20))
+    
+        #resampled = df.resample
+    
+    
+    
+def preperate_data(records):
+    for record in records:
+        
+        ### GPS Preperation ###
+        # preperate_gps(record)
+        
+        ### Sensor Preperation ###
+        preperate_sensor(record)
+    
+def read_labels(file):
+    label = ["", "", ""]
+    
+    # Labels extrahieren
+    split = file.split(".")     # Entfernen der Dateiendung
+    split = split[0].split("_") # Splitten bei _
+    split = split[1:]           # Wegschneiden der Dateinummer
+    
+    try: 
+        label[0] = split[0]
+        label[1] = split[1]
+        label[2] = split[2]
+    except:
+        None
+        
+    return label
+      
+def collect_files(path):
+    records = []
+    
+    # Durchläuft alle User-Ordner
+    for folder in os.listdir(path):
+        # Durchläuft alle Dateien in den User-Ordnern
+        for file in os.listdir(path + folder + "/"):
+            
+            # Prüfe ob Dateiendung "csv" und Datentyp "GPS"
+            if str(file[-7:]) == "GPS.csv":
+                path_gps = path + folder + "/" + file
+                path_sensor = path + folder + "/" + file[:-7] + "SENSOR.csv"
+                
+                labels = read_labels(file)
+                
+                records.append(
+                    Record(
+                        path_gps= path_gps, 
+                        path_sensor= path_sensor, 
+                        label= labels[0],
+                        sublabel= labels[1],
+                        subsublabel= labels[2]
+                    )
+                )
+    
+    return records
+    
 
 # =============================================================================
 # Hauptprogramm
@@ -85,5 +187,13 @@ path = "Z:/2020-JG18-T31Bewegungsanalyse-Pelz-Kroener/05-Messfahrten_Daten/Fireb
 save_path_gps = "Z:/2020-JG18-T31Bewegungsanalyse-Pelz-Kroener/06-Datenaufbereitung/processedData/ml_gps.csv"
 
 if(__name__ == "__main__"):
-    ml_csv = data_import(path)
-    ml_csv.to_csv(save_path_gps)
+    
+    records = collect_files(path)
+    
+    # Anzahl der aufgenommenen Datensätze
+    print(len(records))
+    
+    preperate_data(records)
+    
+    # ml_csv = data_import(path)
+    # ml_csv.to_csv(save_path_gps)
